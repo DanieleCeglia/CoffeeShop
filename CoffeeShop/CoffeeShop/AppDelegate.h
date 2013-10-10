b9d6c23608674437674df914ae05be42b863bb65
Daniele Ceglia <daniele.ceglia@relifeit.com>
2013-10-10 10:07:23 +0200

    progetto creato

---
+++ CoffeeShop/CoffeeShop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoffeeShop
+//
+//  Created by Daniele Ceglia on 10/10/13.
+//  Copyright (c) 2013 Relifeit (Daniele Ceglia). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
